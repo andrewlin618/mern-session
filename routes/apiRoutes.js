5ef41dd1136ae00a03db022ffc48663a4b19499b
Andrew Lin <andrewlin618@gmail.com>
2020-09-28 19:03:37 -0700

    paused other routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,17 @@
 // const path = require("path");
 const router = require("express").Router();
-const users = require("../data/users.json");
-const userController = require("../controllers/userController")
-const { isAdmin, isAuthenticated } = require("../middleware/authMiddleware.js")
+// const users = require("../data/users.json");
+// const userController = require("../controllers/userController")
+// const { isAdmin, isAuthenticated } = require("../middleware/authMiddleware.js")
 
 
 router.route("/")
     .get((req, res) => res.send('API call succeeded!'))
 
-router.route("/users")
-    .get(isAdmin, userController.getAllUsers);
+// router.route("/users")
+//     .get(isAdmin, userController.getAllUsers);
 
-router.route('/user/:username')
-    .get(userController.getUserByUsername);
+// router.route('/user/:username')
+//     .get(userController.getUserByUsername);
 
 module.exports = router;
