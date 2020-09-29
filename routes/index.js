ca1179db284c51fbca0570e1504fd0c00ba22778
Andrew Lin <andrewlin618@gmail.com>
2020-09-28 17:55:25 -0700

    removed front-end

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,20 @@ const path = require("path");
 const router = require("express").Router();
 
 // Import routes; 
-const authRoutes = require("./authRoutes");
 const apiRoutes = require("./apiRoutes");
+const authRoutes = require("./authRoutes");
 
 router.get("/test", (req, res) => {
-    res.send("Test api call succeeded!")
+    res.send("test call succeeded!")
 });
+
 router.use("/api", apiRoutes);
+
 router.use("/auth", authRoutes);
 
 // If no routes are hit, send the React app
-// router.use("*", (req, res) =>
-//     res.sendFile(path.join(__dirname, "../client/build/index.html"))
-// );
+router.use("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "../client/build/index.html"))
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,29 +1,29 @@
 const express = require('express');
 const app = express();
-const cors = require('cors')
-const session = require('express-session');
+// const session = require('express-session');
 const routes = require('./routes');
+// const cors = require('cors')
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
-app.use(cors());
+// app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Session Settings;
-app.use(session({
-    secret: 'some secret word',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 24,
-        secure: false
-    }
-}))
+// app.use(session({
+//     secret: 'some secret word',
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//         maxAge: 1000 * 60 * 60 * 24,
+//         secure: false
+//     }
+// }))
 
 app.use(routes);
 
