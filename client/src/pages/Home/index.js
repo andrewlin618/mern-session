29184e790ded3b8e97ccfe77bea51bd53bd5326c
Andrew Lin <andrewlin618@gmail.com>
2020-09-28 17:26:26 -0700

    debugging

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -17,7 +17,9 @@ const Home = () => {
       <Nav />
       <h1>This is public page</h1>
       <p style={{ color: 'green' }}>You can see this page no matter whether you are logged in or not.</p>
-      <button onClick={handleClick} style={{ backgroundColor: 'red' }}>API CALL</button><br/>
+
+      <button onClick={handleClick}>API CALL</button><br/>
+
       <img src='https://earthsky.org/upl/2018/06/ocean-apr27-2020-Cidy-Chai-North-Pacific-scaled-e1591491800783.jpeg' alt='hello' />
     </>
   );
